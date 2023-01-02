3fc5f8a0bb05aeb20143ad62576b12aeb399c6c3
mactep <tulio@brunoro.org>
2023-01-02 14:38:00 -0300

    WIP: get email from the API request

--- email/internal/transport-inbound/router/handlers.go
+++ email/internal/transport-inbound/router/handlers.go
@@ -1,26 +1,65 @@
 package router
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gothunder/thunder/pkg/router"
+	"github.com/mactep/alternativeco-challenge/email/internal/features"
 )
 
-func NewEmailHandler() router.HandlerOutput {
+func NewEmailHandler(emailService features.EmailService) router.HandlerOutput {
 	return router.HandlerOutput{
-		Handler: emailHandler{},
+		Handler: emailHandler{emailService},
 	}
 }
 
-type emailHandler struct{}
+type emailHandler struct {
+	service features.EmailService
+}
 
 func (h emailHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello email")
+	req := make(map[string]interface{})
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	email, ok := req["email"].(string)
+	if !ok {
+		http.Error(w, "email is not a string", http.StatusBadRequest)
+		return
+	}
+
+	if email == "" {
+		http.Error(w, "no email provided", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	emailRegistry, err := h.service.Create(ctx, email)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error creating email: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// marshal emailRegistry to JSON and write to response
+	jsonEmailRegistry, err := json.Marshal(emailRegistry)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error marshalling emailRegistry: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonEmailRegistry)
 }
 
 func (h emailHandler) Method() string {
-	return "Get"
+	return http.MethodPost
 }
 
 func (h emailHandler) Pattern() string {
